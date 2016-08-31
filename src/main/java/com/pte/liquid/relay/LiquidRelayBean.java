10638103d3b825ace3de69126293300cc18b9e80
rjptegelaar <tegelaarpaul@gmail.com>
2016-08-31 13:39:39 +0200

    Nieuwe poging

--- src/main/java/com/pte/liquid/relay/LiquidRelayBean.java
+++ src/main/java/com/pte/liquid/relay/LiquidRelayBean.java
@@ -14,21 +14,26 @@
 package com.pte.liquid.relay;
 
 import java.util.Properties;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Logger;
 
 import org.apache.camel.Exchange;
 
 import com.pte.liquid.relay.camel.converter.LiquidRelayExchangeConverterImpl;
-import com.pte.liquid.relay.camel.util.LiquidRelayCamelUtil;
 import com.pte.liquid.relay.client.stomp.StompTransport;
 import com.pte.liquid.relay.marshaller.json.JsonMarshaller;
-import com.pte.liquid.relay.model.Message;
 
 public class LiquidRelayBean{
-	
+	private static Logger logger = Logger.getLogger("LiquidRelayBean");
 	
     private Transport transport;
 	private Converter<Exchange> converter;
 	private Marshaller marshaller;
+
+
+	private static final int QUEUE_SIZE = 10000;
+	private static final int THRESHOLD_SIZE = 500;
+	private final LinkedBlockingQueue<Exchange> queue = new LinkedBlockingQueue<Exchange>(QUEUE_SIZE);
 		
     private boolean enabled;
 
@@ -65,38 +70,94 @@ public class LiquidRelayBean{
     	
     }
     
+//	public void process(Exchange exchange) throws Exception {
+//		try{				
+//			
+//			
+//			
+//    		if(enabled){
+//    			    		
+//	        	Message preMsg = converter.convert(exchange);  
+//	        	String correlationID = LiquidRelayCamelUtil.determineCorrelation(exchange);
+//	        	String parentId = LiquidRelayCamelUtil.determineParent(exchange);
+//	        	int order = LiquidRelayCamelUtil.determineOrder(exchange);
+//	        	String messageID = preMsg.getId();
+//	        	
+//	        	LiquidRelayCamelUtil.setCorrelationID(correlationID, exchange);
+//	        	preMsg.setCorrelationID(correlationID);
+//	        	
+//	        	LiquidRelayCamelUtil.setParentID(messageID, exchange);
+//	        	preMsg.setParentID(parentId);
+//	        	
+//	        	LiquidRelayCamelUtil.setOrder(order, exchange);
+//	        	preMsg.setOrder(order);	
+//	        	
+//	        	   	    	    	    	  	   
+//	        	transport.send(preMsg);
+//    		}else{
+//    			//Empty by design
+//    		}
+//    	} catch (Exception e) {
+//			//Empty by design
+//		}
+//		
+//	}
+    
+
+	
 	public void process(Exchange exchange) throws Exception {
 		try{				
+			if(queue.remainingCapacity() <= THRESHOLD_SIZE){
+				logger.warning("Threashold reached, dumping logging message because volume is to high.");
+			}else{
+				logger.info("Processing exchange.");
+				queue.put(exchange);
+			}	
 			
 			
-			
-    		if(enabled){
-    			    		
-	        	Message preMsg = converter.convert(exchange);  
-	        	String correlationID = LiquidRelayCamelUtil.determineCorrelation(exchange);
-	        	String parentId = LiquidRelayCamelUtil.determineParent(exchange);
-	        	int order = LiquidRelayCamelUtil.determineOrder(exchange);
-	        	String messageID = preMsg.getId();
-	        	
-	        	LiquidRelayCamelUtil.setCorrelationID(correlationID, exchange);
-	        	preMsg.setCorrelationID(correlationID);
-	        	
-	        	LiquidRelayCamelUtil.setParentID(messageID, exchange);
-	        	preMsg.setParentID(parentId);
-	        	
-	        	LiquidRelayCamelUtil.setOrder(order, exchange);
-	        	preMsg.setOrder(order);	
-	        	
-	        	   	    	    	    	  	   
-	        	transport.send(preMsg);
-    		}else{
-    			//Empty by design
-    		}
+    		
     	} catch (Exception e) {
 			//Empty by design
 		}
 		
 	}
 
+	public Transport getTransport() {
+		return transport;
+	}
+
+	public void setTransport(Transport transport) {
+		this.transport = transport;
+	}
+
+	public Converter<Exchange> getConverter() {
+		return converter;
+	}
+
+	public void setConverter(Converter<Exchange> converter) {
+		this.converter = converter;
+	}
+
+	public Marshaller getMarshaller() {
+		return marshaller;
+	}
+
+	public void setMarshaller(Marshaller marshaller) {
+		this.marshaller = marshaller;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public LinkedBlockingQueue<Exchange> getQueue() {
+		return queue;
+	}
+	
+	
 
 }
